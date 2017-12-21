ed1cde3a8a3a7434bc5483e475aeeda6e5e215fd
shifraabittan <31713415+shifraabittan@users.noreply.github.com>
2017-12-20 23:42:12 -0500

    Update StudentDatabase.cpp

--- StudentDatabase.cpp
+++ StudentDatabase.cpp
@@ -1,25 +1,42 @@
 #include "StudentDatabase.h"
+#include "MastersStudent.h"
 #include <string>
 #include <stdio.h>
 #include <iostream>
 
+
 // Insert a student into _students map
 void StudentDatabase::addStudent(Student *s) {
   _students[s->getLastName()] = s;
-  // s arrow get last name ->
-  // TODO Implement this
 }
+
+
 
 // Find the student with the matching last_name
 // and call printInfo on that student, or
 // print "<last_name> not found" if the student is not found.
 void StudentDatabase::printStudent(std::string _last_name) {
   // TODO Implement this
+  auto it = _students.find("_last_name");
+  if (_students.count(_last_name) == 0) {
+    std::cout << "<" <<_last_name << "> not found" << std::endl;
+  }
+  else {
+    (**it).printInfo();
+
+    
+  }
+}
+/*
+
   if (_students.count(_last_name) == 0) {
 	std::cout << "<" <<_last_name << "> not found" << std::endl;
   }
   else {
-	printInfo(_students.find("_last_name"));
+      auto it = _students.find("_last_name");
+      (*it)->printInfo();
+      
   }
-
 }
+*/
+
